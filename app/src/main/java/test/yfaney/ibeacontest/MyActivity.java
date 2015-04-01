8326513a222fee7b14bf5d9ac7cd6651893e5c46
Younghwan Jang <yfaney@gmail.com>
2015-04-01 10:35:32 -0500

    Library Update
    beta to 2.1.4

--- app/src/main/java/test/yfaney/ibeacontest/MyActivity.java
+++ app/src/main/java/test/yfaney/ibeacontest/MyActivity.java
@@ -2,26 +2,38 @@ package test.yfaney.ibeacontest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.RemoteException;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
 
 
 public class MyActivity extends Activity implements BeaconConsumer{
     protected static final String TAG = "RangingActivity";
-    private BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+    private BeaconManager beaconManager = null;
+    public final static int BLE_TX_POWER = -56;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+//        beaconManager.getBeaconParsers().add(new BeaconParser().
+//                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:0-3=1AFF7700,i:4-19,i:20-21,i:22-23,p:24-24"));
         beaconManager.bind(this);
     }
     @Override
@@ -51,28 +63,39 @@ public class MyActivity extends Activity implements BeaconConsumer{
 
     @Override
     public void onBeaconServiceConnect() {
-        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+        beaconManager.setRangeNotifier(new RangeNotifier() {
             @Override
-            public void didEnterRegion(Region region) {
-                Log.d(TAG, "I just saw an beacon for the first time!");
-                Toast.makeText(getBaseContext(),"I just saw an beacon for the first time!",Toast.LENGTH_SHORT).show();
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                for (Beacon beacon : beacons) {
+                    String uuid = beacon.getId1().toUuidString();
+                    Log.i(TAG, "The " + uuid + " is about " + getDistance(BLE_TX_POWER, beacon.getRssi()) + " meters away.");
+                    makeToast("The " + uuid + " is about " + getDistance(BLE_TX_POWER, beacon.getRssi()) + " meters away.");
+                }
             }
 
-            @Override
-            public void didExitRegion(Region region) {
-                Log.d(TAG, "I no longer see an beacon");
-                Toast.makeText(getBaseContext(),"I no longer see an beacon",Toast.LENGTH_SHORT).show();
+            private void makeToast(final String msg) {
+                // create a handler to post messages to the main thread
+                Handler mHandler = new Handler(getMainLooper());
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
 
-            @Override
-            public void didDetermineStateForRegion(int state, Region region) {
-                Log.d(TAG, "I have just switched from seeing/not seeing beacons: " + state);
-                Toast.makeText(getBaseContext(),"I have just switched from seeing/not seeing beacons: " + state,Toast.LENGTH_SHORT).show();
-            }
         });
 
         try {
-            beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
-        } catch (RemoteException e) {   }
+            beaconManager.startRangingBeaconsInRegion(new Region("LondonTracker", null, null, null));
+            Log.d(TAG, "Start Montoring!!");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+    private static double getDistance(double calibrated_power, double rssi){
+        double ratio_db = calibrated_power - rssi;
+        double ratio_linear = Math.pow(10, ratio_db / 10);
+        return Math.sqrt(ratio_linear);
     }
 }
